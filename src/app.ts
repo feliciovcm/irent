5cc5ceebe4a918d96e660d2eb45be5cd08c3394c
Vitor Felicio Costa Moreira <vitor@pop-os.localdomain>
2022-03-31 14:51:45 -0300

    Sentry

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 // use dotenv lib to insert environment variables into application
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
@@ -20,6 +22,18 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app })
+  ],
+
+  tracesSampleRate: 1.0
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -28,6 +42,8 @@ app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
